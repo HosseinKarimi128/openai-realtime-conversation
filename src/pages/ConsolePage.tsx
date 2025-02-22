0a759dca4f08b48ab97f54bed3fd1e336f5add7a
hosseinkarimi128 <hossein.karimi.0128@gmail.com>
2025-02-22 19:21:23 +0330

    remove logging formating logice

--- src/pages/ConsolePage.tsx
+++ src/pages/ConsolePage.tsx
@@ -125,27 +125,7 @@ export function ConsolePage() {
   });
   const [marker, setMarker] = useState<Coordinates | null>(null);
 
-  /**
-   * Utility for formatting the timing of logs
-   */
-  const formatTime = useCallback((timestamp: string) => {
-    const startTime = startTimeRef.current;
-    const t0 = new Date(startTime).valueOf();
-    const t1 = new Date(timestamp).valueOf();
-    const delta = t1 - t0;
-    const hs = Math.floor(delta / 10) % 100;
-    const s = Math.floor(delta / 1000) % 60;
-    const m = Math.floor(delta / 60_000) % 60;
-    const pad = (n: number) => {
-      let s = n + '';
-      while (s.length < 2) {
-        s = '0' + s;
-      }
-      return s;
-    };
-    return `${pad(m)}:${pad(s)}.${pad(hs)}`;
-  }, []);
-
+ 
   /**
    * When you click the API key
    */
